2bfba37167a7d2e7d4341d9eca23f786293e0b38
MdotKvist <Kvist339@gmail.com>
2024-02-19 10:16:02 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -1,9 +1,2 @@
-﻿string firstName;
-firstName = "Bob";
-Console.WriteLine(firstName);
-firstName = "Liem";
-Console.WriteLine(firstName);
-firstName = "Isabella";
-Console.WriteLine(firstName);
-firstName = "Yasmin";
+﻿string firstName = "Bob";
 Console.WriteLine(firstName);
