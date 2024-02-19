f796416b459735f0421745e4fe0df41b895a4d8e
MdotKvist <Kvist339@gmail.com>
2024-02-19 12:05:50 +0100

    Update Program.cs

--- Dag 1.2 - Guided project - Calculate and print student grades/Program.cs
+++ Dag 1.2 - Guided project - Calculate and print student grades/Program.cs
@@ -24,3 +24,20 @@ int jeong2 = 92;
 int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
+
+
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+decimal sophiaScore;
+decimal nicolasScore;
+decimal zahirahScore;
+decimal jeongScore;
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahriah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
+
