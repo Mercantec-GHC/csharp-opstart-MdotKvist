4148b60d483cb0b3acdb721e1df177e639af4c68
MdotKvist <Kvist339@gmail.com>
2024-02-19 09:27:04 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -1,2 +1,5 @@
 ﻿Console.WriteLine("Hello World");
 // Console.WriteLine("Hello World");
+Console.WriteLine("Congratulations!");
+Console.WriteLine(" ");
+Console.WriteLine("You wrote your first line");
