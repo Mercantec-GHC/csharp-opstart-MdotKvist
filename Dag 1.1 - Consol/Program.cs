952bd83d66ea6f1ae286288d121731e449808cf2
MdotKvist <Kvist339@gmail.com>
2024-02-19 10:50:56 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -1,2 +1,10 @@
-﻿Console.WriteLine("Hello\nWorld!");
-Console.WriteLine("Hello\tWorld!");
+﻿Console.WriteLine("Generating invoices for customer \"Contonso Corp\" ...\n");
+
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+
+Console.WriteLine(@"c:\source\repos
+        (this is where your code goes)");
+
+Console.Write(@"c:\invoices");
