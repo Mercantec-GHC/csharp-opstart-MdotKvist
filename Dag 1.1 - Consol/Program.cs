f0f92e6fcfc5486a47b8f7b9f879b63ff1ade358
MdotKvist <Kvist339@gmail.com>
2024-02-19 11:31:41 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
-Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+﻿int fahrenheit = 94;
+decimal celcius = (fahrenheit - 32m) * (5.0m / 9.0m);
+
+Console.WriteLine($"The temperature is {celcius} Celsius.");
