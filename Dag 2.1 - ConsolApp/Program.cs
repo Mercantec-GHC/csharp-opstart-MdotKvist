9f62192538e0b9202702cad4160235983a097169
MdotKvist <Kvist339@gmail.com>
2024-02-20 09:29:44 +0100

    Did you win or lose?

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -8,6 +8,16 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
+if ( total > 14 )
+{
+    Console.WriteLine("You win");
+}
+
+else if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
+
 int firstValue = 500;
 int secondValue = 600;
 int largerValue = System.Math.Max( firstValue, secondValue );
