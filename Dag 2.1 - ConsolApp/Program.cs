0fa58eea6b3031add10ae8b1387b6e7373f460e5
MdotKvist <Kvist339@gmail.com>
2024-02-20 09:51:47 +0100

    Doubles and triples?

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -13,12 +13,20 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
     Console.WriteLine("You rolled doubles! +2 bonus to total!");
     total += 2;
 }
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+
 if ( total > 14 )
 {
     Console.WriteLine("You win");
 }
 
-else if (total < 15)
+if (total < 15)
 {
     Console.WriteLine("Sorry, you lose.");
 }
