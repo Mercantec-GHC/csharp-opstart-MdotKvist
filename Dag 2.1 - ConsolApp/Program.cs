71a366db082b8482ca720293bacc6af122b97f12
MdotKvist <Kvist339@gmail.com>
2024-02-20 10:16:04 +0100

    Program challenges

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -1,4 +1,6 @@
-﻿Random dice = new Random();
+﻿using System.ComponentModel.Design;
+
+Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
@@ -55,7 +57,47 @@ string message = "The quick brown fox jumps over the lazy dog.";
 bool result = message.Contains("dog");
 Console.WriteLine(result);
 
-if (message.Contains("fox")) ;
+if (message.Contains("fox"))
 {
     Console.WriteLine("What does the fox say?");
 }
+
+
+//white space
+
+Console.WriteLine("\n\n");
+
+
+
+
+
+
+
+
+Random random = new Random();
+int daysUntilExpiration = 1; //random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+
+
+else if (daysUntilExpiration <= 1)
+{
+    Console.WriteLine(@"Your subscription expires with in a day!
+Renew now and save 20%!");
+}
+
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine(@$"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save 10%!");
+}
+
+else if (daysUntilExpiration <= 10 )
+{
+    Console.WriteLine("Your subscription will expire soon. renew now!");
+}
+
