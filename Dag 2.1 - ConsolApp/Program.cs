f8a5a806b3a5f5ef803efda597399d3ef336709d
MdotKvist <Kvist339@gmail.com>
2024-02-20 11:21:02 +0100

    Style guidelines

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -1,232 +1,34 @@
-﻿using System.ComponentModel.Design;
-
-Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) && (roll2 == roll3))
-{
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-}
-
-else
-{
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-}
-
-
-if ( total >= 16 )
-{
-    Console.WriteLine("You win a new car!");
-}
-
-else if ( total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
-}
-
-else if ( total == 7)
-{
-    Console.WriteLine("You win a trip for two!");
-}
-
-else
-{
-    Console.WriteLine("You win a kitten!");
-}
-
-
-
-
-
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = System.Math.Max( firstValue, secondValue );
-
-Console.WriteLine(largerValue);
-
-string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
-
-if (message.Contains("fox"))
-{
-    Console.WriteLine("What does the fox say?");
-}
-
-
-//white space
-
-Console.WriteLine("\n\n");
-
-
-
-
-
-
-
-
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
-}
-
-
-Console.WriteLine("\n\n");
-
-
-
-
-/*
-string[] fraudulentOrderIDs = new string[3];
-
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
-*/
-
-string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
-
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
-fraudulentOrderIDs[0] = "F000";
-
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
-
-
-
-//WHITESPACE
-Console.WriteLine("\n\n");
-
-
-
-int[] inventory = { 200, 450, 700, 175, 250 };
-int sum = 0;
-int bin = 0;
-foreach (int items in inventory)
-{
-    sum += items;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-}
-
-Console.WriteLine($"We have {sum} item in inventory.");
-
-
-
-
-//WhiteSpace
-Console.WriteLine("\n\n");
-
-
-string[] fraudulenOrderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-foreach (string id  in fraudulenOrderID)
-{
-    if (id.StartsWith("B"))
-    {
-        Console.WriteLine($"{id}");
-    }
-}
-
-
-
-//WHITHESPACE
-Console.WriteLine("\n\n");
-
-
-
-
-Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
-}
-
-
-
-
-
-//WHITHESPACE
-Console.WriteLine("\n\n");
-
-
-
-
-
-
-string str = "The quick brown fox jumps over the lazy dog.";
+﻿
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
 
 
 // convert the message into a char array
-char[] charMessage = str.ToCharArray();
+char[] message = originalMessage.ToCharArray();
 
 
 // Reverse the chars
-Array.Reverse(charMessage);
-int x = 0;
+Array.Reverse(message);
+
+int lettercount = 0;
 
 
 // count the o's
-foreach (char i in charMessage) { if (i == 'o') { x++; } }
+foreach (char letter in message) 
+{
+    if (letter == 'o') 
+    {
+        lettercount++;
+    }
+}
 
 
 // convert it back to a string
-string new_message = new String(charMessage);
+string newMessage = new String(message);
 
 
 // print it out
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {lettercount} times.");
+
+
+
