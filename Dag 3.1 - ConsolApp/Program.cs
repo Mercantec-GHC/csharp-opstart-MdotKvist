5bc9512d68a34b8194fd722d77b7683d6c88c148
MdotKvist <Kvist339@gmail.com>
2024-02-21 09:42:49 +0100

    Lose the if statements!

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -8,15 +8,8 @@ for (int i = 0; i < coinsTotal; i++)
 
     int randomNumber = random.Next(0, 2);
 
-    int result = (randomNumber == 0) ? 0 : 1;
+    string result = (randomNumber == 0) ? "HEAD" : "TALE";
 
-    if (result == 0)
-    {
-        Console.WriteLine("Congratulations, you flipped the coin and got a Head");
-    }
-    else if (result == 1)
-    {
-        Console.WriteLine("congratulations, you flipped the coin and got a Tale");
-    }
+    Console.WriteLine($"You flipped a {result}!");
 }
 
