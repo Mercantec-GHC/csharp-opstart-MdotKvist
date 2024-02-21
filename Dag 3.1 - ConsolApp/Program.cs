3a4f959ded870412f303e8f1fd8d27e7d08c99af
MdotKvist <Kvist339@gmail.com>
2024-02-21 13:18:07 +0100

    Login input

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -220,3 +220,24 @@
 //    Console.WriteLine("Hero wins!");
 //}
 
+using System;
+
+        int userInput;
+        bool isValid = false;
+
+        do
+        {
+            Console.WriteLine("Indtast et heltal:");
+            string input = Console.ReadLine();
+
+            isValid = int.TryParse(input, out userInput);
+
+            if (!isValid)
+            {
+                Console.WriteLine("Ugyldigt input. Prøv igen.");
+            }
+
+        } while (!isValid);
+
+        Console.WriteLine("Du har indtastet: " + userInput);
+
