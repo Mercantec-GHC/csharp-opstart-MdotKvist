85409ae2ed544c11a3dd3664d06f76a2d392b6d2
MdotKvist <Kvist339@gmail.com>
2024-02-21 12:07:30 +0100

    Code challenge - implement the FizzBuzz challenge rules

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -141,17 +141,37 @@
 
 //Console.WriteLine($"Product: {size} {color} {type}");
 
-string[] names = { "Alex", "Eddie", "David", "Michael" };
+//string[] names = { "Alex", "Eddie", "David", "Michael" };
 
-for (int i = 0; i < names.Length; i++)
-{
-    if (names[i] == "David")
+//for (int i = 0; i < names.Length; i++)
+//{
+//    if (names[i] == "David")
+//    {
+//        names[i] = "Sammy";
+//    }
+//}
+
+//foreach (var name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+
+
+for (int i = 1; i < 101; i++){
+    if (i % 3 == 0 && (i % 5 == 0)  )
     {
-        names[i] = "Sammy";
+        Console.WriteLine($"{i} - FizzBuss");
     }
-}
-
-foreach (var name in names)
-{
-    Console.WriteLine(name);
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} - Fizz");       
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else { 
+        Console.WriteLine(i);
+    }
 }
