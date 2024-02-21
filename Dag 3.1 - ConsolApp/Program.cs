385f25959740e0a4c93fc420b5fd98456ff9a5aa
MdotKvist <Kvist339@gmail.com>
2024-02-21 11:03:12 +0100

    Code challenge: rewrite if-elseif-else using a switch statement Done

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -57,23 +57,86 @@
 //Console.WriteLine($"Outside the code block: {value}");
 
 
-using System;
+//using System;
 
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
+//int[] numbers = { 4, 8, 15, 16, 23, 42 };
 
-int total = 0;
+//int total = 0;
 
-foreach (int number in numbers)
+//foreach (int number in numbers)
+//{
+
+
+//    total += number;
+
+//    if (number == 42)
+//    {
+//        Console.WriteLine("Set contains 42");
+//    }
+
+//}
+
+//Console.WriteLine($"Total: {total}");
+
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
+    case "01":
+        type = "Sweat shirt";
+        break;
 
+    case "02":
+        type = "T-shirt";
+        break;
 
-    total += number;
+    case "03":
+        type = "Sweat pants";
+        break;
 
-    if (number == 42)
-    {
-        Console.WriteLine("Set contains 42");
-    }
-
+    default:
+        type = "Other";
+        break;
 }
 
-Console.WriteLine($"Total: {total}");
+switch (product[1]) { 
+
+    case "BL":
+        color = "Black";
+        break;
+
+    case "MN":
+        color = "Maroon";
+        break;
+
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+
+    case "M":
+        size = "Medium";
+        break;
+
+    case "L":
+        size = "Large";
+        break;
+
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
