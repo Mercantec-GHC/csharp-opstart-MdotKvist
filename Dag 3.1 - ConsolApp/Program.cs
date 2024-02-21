2d5ae7aaaea5d5cae979dc91bcd48420bb6e52f3
MdotKvist <Kvist339@gmail.com>
2024-02-21 11:47:19 +0100

    Loop trial

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -141,8 +141,17 @@
 
 //Console.WriteLine($"Product: {size} {color} {type}");
 
-for (int i = 0; i < 10; i++)
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = 0; i < names.Length; i++)
 {
-    Console.WriteLine(i);
-    if (i == 7) break;
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
 }
