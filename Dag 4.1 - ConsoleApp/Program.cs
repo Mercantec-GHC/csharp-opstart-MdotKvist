6b0a21b82ee77124ee4aa6ecfddece6938796f66
MdotKvist <Kvist339@gmail.com>
2024-02-22 08:44:51 +0100

    combining string array values as strings and as integers challenge

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -45,3 +45,40 @@
 
 
 
+//string value = "bad";
+//int result = 0;
+//if (int.TryParse(value, out result))
+//{
+//    Console.WriteLine($"Measurement: {result}");
+//}
+//else
+//{
+//    Console.WriteLine("Unable to report the measurement.");
+//}
+
+//if (result > 0)
+//    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
+
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
