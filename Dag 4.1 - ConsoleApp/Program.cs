54c28e2077656d14dfc322b8a30a50b01c97e87f
MdotKvist <Kvist339@gmail.com>
2024-02-22 12:25:50 +0100

    Round formatting

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -283,3 +283,15 @@
 
 //yourDiscount += $"A discount of {((price - salePrice) / price):P2}!"; //inserted
 //Console.WriteLine(yourDiscount);
+
+
+//int invoiceNumber = 1201;
+//decimal productShares = 25.4568m;
+//decimal subtotal = 2750.00m;
+//decimal taxPercentage = .15825m;
+//decimal total = 3185.19m;
+
+//Console.WriteLine($"Invoice Number: {invoiceNumber}");
+//Console.WriteLine($"   Shares: {productShares:N3} Product");
+//Console.WriteLine($"     Sub Total: {subtotal:C}");
+//Console.WriteLine($"           Tax: {taxPercentage:P2}");
