85d4da86d8456a1745679cfde2e898ee243748de
MdotKvist <Kvist339@gmail.com>
2024-02-22 10:02:53 +0100

    string.join

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -200,8 +200,16 @@
 //}
 
 
+//string value = "abc123";
+//char[] valueArray = value.ToCharArray();
+//Array.Reverse(valueArray);
+//string result = new string(valueArray);
+//Console.WriteLine(result);
+
+
 string value = "abc123";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
-string result = new string(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
 Console.WriteLine(result);
