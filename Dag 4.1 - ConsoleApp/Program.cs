e2eeede6952e07c67f556f41e8ee5016c4878871
MdotKvist <Kvist339@gmail.com>
2024-02-22 12:34:01 +0100

    Strings padding

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -296,6 +296,16 @@
 //Console.WriteLine($"     Sub Total: {subtotal:C}");
 //Console.WriteLine($"           Tax: {taxPercentage:P2}");
 
-string input = "Pad this";
-Console.WriteLine(input.PadLeft(12, '-'));
-Console.WriteLine(input.PadRight(12, '-'));
+//string input = "Pad this";
+//Console.WriteLine(input.PadLeft(12, '-'));
+//Console.WriteLine(input.PadRight(12, '-'));
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
