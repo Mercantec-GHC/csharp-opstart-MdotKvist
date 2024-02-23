eac368a0d313b39601b1a83ed0bae83c9346a14b
MdotKvist <Kvist339@gmail.com>
2024-02-23 10:04:22 +0100

    name function

--- Dag 5.1 - ConsoleApp/Program.cs
+++ Dag 5.1 - ConsoleApp/Program.cs
@@ -166,28 +166,42 @@
 //    }
 //}
 
-int[] schedule = { 800, 1200, 1600, 2000 };
+//int[] schedule = { 800, 1200, 1600, 2000 };
 
-void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+//void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+//{
+//    int diff = 0;
+//    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//    {
+//        Console.WriteLine("Invalid GMT");
+//    }
+//    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//    {
+//        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//    }
+//    else
+//    {
+//        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//    }
+//    for (int i = 0; i < times.Length; i++)
+//    {
+//        int newTime = ((times[i] + diff)) % 2400;
+//        Console.WriteLine($"{times[i]} -> {newTime}");
+//    }
+//}
+
+//DisplayAdjustedTimes(schedule, 6, -6);
+
+string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+DisplayStudents(students);
+DisplayStudents(new string[] { "Robert", "Vanya" });
+
+void DisplayStudents(string[] students)
 {
-    int diff = 0;
-    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    foreach (string student in students)
     {
-        Console.WriteLine("Invalid GMT");
+        Console.Write($"{student}, ");
     }
-    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-    {
-        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-    }
-    else
-    {
-        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-    }
-    for (int i = 0; i < times.Length; i++)
-    {
-        int newTime = ((times[i] + diff)) % 2400;
-        Console.WriteLine($"{times[i]} -> {newTime}");
-    }
+    Console.WriteLine();
 }
-
-DisplayAdjustedTimes(schedule, 6, -6);
